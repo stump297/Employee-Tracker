09e5eb6c2e18db402e4a4f136293973431e2fcd9
Adam <aorf@femmeosage.net>
2024-08-27 11:46:02 -0500

    setting up main menu

--- server.js
+++ server.js
@@ -10,3 +10,51 @@ const pool = new Pool({
   database: "employee_db",
   password: "",
 });
+
+client.connect();
+
+const mainMenu = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        default:
+          client.end();
+      }
+    });
+};
